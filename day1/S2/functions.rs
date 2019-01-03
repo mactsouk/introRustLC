2f9c8f1ff9699705a14d9b62a57e773743591678
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-01-04 00:40:09 +0200

    Adding more Rust code

---
+++ day1/S2/functions.rs
@@ -0,0 +1,18 @@
+
+fn bigger(a: u32, b:i64) -> i64 {	
+	let mut result = a as i64;
+	if result < b {
+		result = b;
+	}
+	
+	// return result;
+	result
+}
+
+fn main() {
+	let a = bigger(10, -200);
+	let b = bigger(100, 2);
+	
+	println!("a: {}", a);
+	println!("b: {}", b);
+}
