6aa6787052978bdedc500e4235a583b2c812a959
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-01-03 20:10:30 +0200

    Get + Set env var

--- day1/S2/env_variables.rs
+++ day1/S2/env_variables.rs
@@ -5,4 +5,11 @@ fn main() {
         Ok(value) => println!("Current path: {}", value),
         Err(e) => println!("PATH NOT SET {})", e),
     };
+
+	let my_env = "MIHALIS";
+	env::set_var(my_env, "My Value");
+    match env::var("MIHALIS") {
+        Ok(value) => println!("Current path: {}", value),
+        Err(e) => println!("MIHALIS NOT SET {})", e),
+    };
 }
