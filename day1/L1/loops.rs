175b1bd70c87f3ce8ed194fec0a0dce2944ff013
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-01-04 17:00:02 +0200

    Added loop examples

--- day1/L1/loops.rs
+++ day1/L1/loops.rs
@@ -1,4 +1,37 @@
 
 fn main() {
+	let mut i: i32 = 0;
+	loop {
+		println!("i: {}", i);
+		i += 1;
+		if i == 10 {
+			break;
+		}
+	}
+	println!("Outside i: {}", i);
+	
+	// Iterator
+	// The .. operator is _Range_
+	// Ranges are iretable types because they
+	// implement the std::iter::IntoIterator _trait_.
+	for xx in 0..10 {
+		println!("xx: {}", xx);
+	}
+	
+	i = 0;
+	while i < 10 {
+		println!("i: {}", i);
+		i += 1;
+	}
+	println!("Outside i: {}", i);
 
+	// Using a Vector with various values
+	let numbers = vec![0, 4, -10];
+    for i in 1..4 {
+        for j in numbers.iter() {
+            let _: &i32 = j;
+            println!("i:{} j:{}", i, j);
+        }
+    }
+	
 }
