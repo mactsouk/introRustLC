26f478b4bb7bd6319baa3d80f02c28351c316138
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-01-04 22:33:37 +0200

    arrays.rs ready

--- day1/S2/arrays.rs
+++ day1/S2/arrays.rs
@@ -1,6 +1,25 @@
 
 fn main() {
-	
-	
-	
+    // Fixed-size array
+    let a1: [i64; 4] = [-1, 1, 2, 3];
+
+    // Initialiazing an array
+    let a2: [i32; 1500] = [0; 1500];
+
+    // Indexing starts at 0
+    println!("First element: {}", a1[0]);
+    println!("Second element: {}", a1[1]);
+
+	// len() for array length
+    println!("Array size: {}", a2.len());
+
+    // Create a slice from an array
+    let s1 = &a2[3 .. 10];
+	println!("{:?}", s1);
+
+	// len() for slice length
+    println!("Slice size: {}", s1.len());
+
+    // Error:
+    // println!("{}", a1[5]);
 }
