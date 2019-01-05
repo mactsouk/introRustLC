379b315f4a2d06e239403111accf2674fb8d32d8
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-01-05 10:38:59 +0200

    More examples in functions.rs

--- day1/S2/functions.rs
+++ day1/S2/functions.rs
@@ -9,6 +9,10 @@ fn bigger(a: u32, b:i64) -> i64 {
 	result
 }
 
+fn check_type(a: i32) {
+	println!("a: {}", a);
+}
+
 fn main() {
 	let a = bigger(10, -200);
 	let b = bigger(100, 2);
@@ -16,4 +20,32 @@ fn main() {
 	println!("a: {}", a);
 	println!("b: {}", b);
 	
+	let x: i64 = 10;
+	// This is OK
+	check_type(x as i32);
+	// This is not OK!
+	// check_type(x);
+	
+	let xx = 10;
+	println!("Outside xx: {}", xx);
+	
+	let yy = {
+		let xx = 15;
+		println!("xx: {}", xx);
+		xx + 10
+	};
+	println!("yy: {}", yy);
+
+	// This is different!
+	let yyy = {
+		let xx = 15;
+		println!("xx: {}", xx);
+		// This where the problem is
+		xx + 10;
+	};
+
+	// This is different!
+	println!("yyy: {:?}", yyy);
+	
+	println!("Outside xx: {}", xx);
 }
