4ed380b651c1c895f1a23a84fabf5b8134abb118
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-01-05 20:27:01 +0200

    Tabs to spaces

--- day1/S2/env_variables.rs
+++ day1/S2/env_variables.rs
@@ -6,8 +6,8 @@ fn main() {
         Err(e) => println!("PATH NOT SET {})", e),
     };
 
-	let my_env = "MIHALIS";
-	env::set_var(my_env, "My Value");
+    let my_env = "MIHALIS";
+    env::set_var(my_env, "My Value");
     match env::var("MIHALIS") {
         Ok(value) => println!("Current path: {}", value),
         Err(e) => println!("MIHALIS NOT SET {})", e),
