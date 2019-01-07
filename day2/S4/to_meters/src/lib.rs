835f56578f8b5f6893e9300da09c70d7da9e4081
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-01-07 20:14:30 +0200

    Define a Trait

---
+++ day2/S4/to_meters/src/lib.rs
@@ -0,0 +1,39 @@
+// This is just a tutple
+#[derive(PartialEq,Debug)]
+pub struct METERS (f32);
+#[derive(PartialEq,Debug)]
+pub struct KILOMETERS (f32);
+#[derive(PartialEq,Debug)]
+pub struct MILES (f32);
+
+pub trait ToMeters {
+	fn to_meters(&self)->METERS;
+}
+
+impl ToMeters for KILOMETERS {
+	fn to_meters(&self)->METERS {
+		METERS(self.0 * 1000.0)
+	}
+}
+
+impl ToMeters for MILES {
+	fn to_meters(&self)->METERS {
+		METERS(self.0 * 1609.0)
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+    #[test]
+    fn it_works() {
+		// What is going to happen if you use the following?
+		// let kilo = KILOMETERS(10);
+        let kilo = KILOMETERS(10.0);
+		let meters = kilo.to_meters();
+		assert_eq!(meters, METERS(10000.0));
+        let miles = MILES(10.0);
+		let meters_miles = miles.to_meters();
+		assert_eq!(meters_miles, METERS(16090.0));
+    }
+}
